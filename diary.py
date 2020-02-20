44807eeb717014d3fc998e02c4186eaf36f2c94a
Linnea Karlström <linnea.karlstrom@skola.botkyrka.se>
2020-02-20 11:51:57 +0100

    working

--- diary.py
+++ diary.py
@@ -4,10 +4,10 @@ import webbrowser
 date = input("What date is it today? [dd-mm-yy] \n")
 rate = input("Can you rate your feeling on a scale 1-10 \n")
 feeling = input("How are you feeling today? \n")
-sleep = input("How did you sleep last night?")
 
 note = input("Can you tell me why? Did something happened? \n")
-
+sleep = input("How did you sleep last night?")
+sleeping =input("How much did you sleep?")
 con = input("Do you want to tell me something else? [yes/no] \n")
 
 if con == "yes":
@@ -20,8 +20,8 @@ Questions = [
     [date],
     ["Can you rate your feeling on a scale 1-10 ", rate],
     ["How are you feeling today? ", feeling],
-    ["How much sleep did you get last night? "]
     ["Can you tell me why? Did something happened? ", note],
+    ["How did you sleep last night?", sleep, sleeping],
     ["Do you want to tell me something else? ", con, note2],
     [" "]
 ]
