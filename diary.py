194ed7cfe2ca155b6339bdabcab0b8c9855e261c
Linnea Karlström <linnea.karlstrom@skola.botkyrka.se>
2020-03-26 11:01:30 +0100

    inloggning fixad

--- diary.py
+++ diary.py
@@ -1,6 +1,32 @@
 import csv
 
-# Inloggning?
+def create_user():
+    new_username = input("Choose your username: \n")
+    new_password = input("Choose your password: \n")
+    with open('login_data.csv', 'a', newline='') as csvfile:
+        fieldnames = ['Username', 'Password']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writerow({fieldnames[0]: new_username, fieldnames[1]: new_password})
+    login()
+
+def user_exists(username, password):
+    with open('login_data.csv','r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            if username == row['Username'] and password == row['Password']:
+                return True
+            else:
+                return False
+
+def login():
+    username = input("Enter your username: \n")
+    password = input("Enter your password: \n")
+    if user_exists(username, password):
+        quiz.start()
+    else:
+        create_user()
+
+login()
 
 read_write = input("Do you want to read in your diary or write? [read / write] \n")
 questions = open("questions.txt", "r").read().split(',')
