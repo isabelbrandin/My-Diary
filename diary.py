bcd023dd8447189d9d7a6f07044e8ef67ebb59b2
Linnea Karlström <linnea.karlstrom@skola.botkyrka.se>
2020-04-09 11:19:05 +0200

    it's smarter now

--- diary.py
+++ diary.py
@@ -2,13 +2,14 @@ import csv
 import webbrowser
 import random
 import more
+from datetime import date
 
 def run():
     read_write = input("Do you want to read in your diary or write? [read / write] \n")
     questions = open("questions.txt", "r").read().split(',')
 
     if read_write == "write":
-        date = input("What date is it today? [dd-mm-yy] \n")
+        datee = date.today()
         
         feeling = input(questions[0] + '\n')
         rate = input(questions[1] + '\n')
@@ -24,18 +25,18 @@ def run():
             print("\nOkay, remember that i'm always here for you! I see you tomorrow.\n")
 
         with open('diary.csv', 'a', newline='') as file:
-            fieldnames = ['date', 'feeling', 'rate', 'note', 'sleep', 'naptime', 'con', 'note2']
+            fieldnames = ['datee', 'feeling', 'rate', 'note', 'sleep', 'naptime', 'con', 'note2']
             writer = csv.DictWriter(file, fieldnames=fieldnames)
             # writer.writeheader()
             if note2 != ' ':
-                writer.writerow({fieldnames[0]: date, fieldnames[1]: feeling, fieldnames[2]: rate, fieldnames[3]: note, fieldnames[4]: sleep, fieldnames[5]: naptime, fieldnames[6]: con, fieldnames[7]: note2})
+                writer.writerow({fieldnames[0]: datee, fieldnames[1]: feeling, fieldnames[2]: rate, fieldnames[3]: note, fieldnames[4]: sleep, fieldnames[5]: naptime, fieldnames[6]: con, fieldnames[7]: note2})
             else:
-                writer.writerow({fieldnames[0]: date, fieldnames[1]: feeling, fieldnames[2]: rate, fieldnames[3]: note, fieldnames[4]: sleep, fieldnames[5]: naptime, fieldnames[6]: con})
+                writer.writerow({fieldnames[0]: datee, fieldnames[1]: feeling, fieldnames[2]: rate, fieldnames[3]: note, fieldnames[4]: sleep, fieldnames[5]: naptime, fieldnames[6]: con})
 
 
     elif read_write == 'read':
 
-        day = input('Which day do you want to read? [dd-mm-yy] \n')
+        day = input('Which day do you want to read? [yyyy-mm-dd] \n')
         
         with open('diary.csv', 'r') as file:
             reader = csv.DictReader(file)
